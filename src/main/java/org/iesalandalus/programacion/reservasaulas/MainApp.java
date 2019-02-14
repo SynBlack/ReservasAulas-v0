5b2742f5446c0ecc7d6a9ff45ca163e2d3eac949
Darkcrow <Darkcrow@192.168.1.102>
2019-02-14 11:00:07 +0100

    Creación de la clase Main.

--- src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
@@ -1,9 +1,19 @@
 package org.iesalandalus.programacion.reservasaulas;
 
+import org.iesalandalus.programacion.reservasaulas.vista.Consola;
+import org.iesalandalus.programacion.reservasaulas.vista.IUTextual;
+
+/**
+ * @author Emanuel Martínez Lonardi
+ *
+ */
+
 public class MainApp {
 
 	public static void main(String[] args) {
-		System.out.println("Programa para la gestión de reservas de espacios del IES Al-Ándalus");
+		Consola.mostrarCabecera("Gestión de reservas de Aulas - IES Al-Ándalus");
+		IUTextual iuTextual = new IUTextual();
+		iuTextual.comenzar();
 	}
 
 }
